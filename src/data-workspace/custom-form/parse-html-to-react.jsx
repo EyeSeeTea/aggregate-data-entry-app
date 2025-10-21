73e1eced1f69ed979b0422d8810e52236e307d0b
gqcorneby <185756521+gqcorneby@users.noreply.github.com>
2025-10-21 18:29:04 +0800

    update comment

--- src/data-workspace/custom-form/parse-html-to-react.jsx
+++ src/data-workspace/custom-form/parse-html-to-react.jsx
@@ -12,8 +12,7 @@ export const parseHtmlToReact = (htmlCode, metadata) =>
                 case 'td':
                     return replaceTdNode(domNode, metadata)
                 case 'script':
-                    // Always allow scripts to pass through, but don't let html-react-parser
-                    // handle them inconsistently - we'll execute them manually in CustomForm
+                    // Always allow scripts to pass through, but execute them manually in CustomForm
                     return undefined
                 default:
                     return undefined
